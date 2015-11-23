6a77db07799208119fe300a8963fd20f47b922ce
Jeff Garzik <jeff@bloq.com>
2015-11-23 03:46:20 +0000

    fortune-client: convert to click CLI API

--- fortune/fortune-client.py
+++ fortune/fortune-client.py
@@ -9,6 +9,7 @@
 import json
 import os
 import sys
+import click
 
 # import from the 21 Developer Library
 from two1.commands.config import Config
@@ -20,22 +21,48 @@ wallet = Wallet()
 username = Config().username
 requests = BitTransferRequests(wallet, username)
 
-# server address
-server_url = 'http://localhost:12012/'
+FORTUNECLI_VERSION = '1.0'
+DEFAULT_ENDPOINT = 'http://localhost:12012/'
 
-def cmd_fortune():
-    sel_url = server_url + 'fortune'
+@click.group(invoke_without_command=True)
+@click.option('--endpoint', '-e',
+              default=DEFAULT_ENDPOINT,
+              metavar='STRING',
+              show_default=True,
+              help='API endpoint URI')
+@click.option('--debug', '-d',
+              is_flag=True,
+              help='Turns on debugging messages.')
+@click.version_option(FORTUNECLI_VERSION)
+@click.pass_context
+def main(ctx, endpoint, debug):
+    """ Command-line Interface for the fortune cookie service
+    """
+
+    if ctx.obj is None:
+        ctx.obj = {}
+
+    ctx.obj['endpoint'] = endpoint
+
+    if ctx.invoked_subcommand is None:
+        cmd_fortune(ctx)
+
+
+def cmd_fortune(ctx):
+    sel_url = ctx.obj['endpoint'] + 'fortune'
     answer = requests.get(url=sel_url.format())
     print(answer.text)
 
-def cmd_info():
-    sel_url = server_url
+
+@click.command(name='info')
+@click.pass_context
+def cmd_info(ctx):
+    sel_url = ctx.obj['endpoint']
     answer = requests.get(url=sel_url.format())
     print(answer.text)
 
-if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1] == "info":
-        cmd_info()
-    else:
-        cmd_fortune()
+main.add_command(cmd_info)
+
+if __name__ == "__main__":
+    main()
 
